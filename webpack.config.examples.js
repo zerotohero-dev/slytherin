16e8f8ce720ac737bf6465a7ef0f12840f6b1fbb
Volkan Özçelik <volkan.ozcelik@gmail.com>
2018-01-04 23:38:39 -0800

    Update webpack.config.examples.js

--- webpack.config.examples.js
+++ webpack.config.examples.js
@@ -1,5 +1,16 @@
-/*
+/*    _    _
+ * ,-(|)--(|)-.
+ * \_   ..   _/ Slytherin: Framework-independent slithering utility.
+ *   \______/              Makes things draggable.
+ *     V  V
  *
+ * This project is a part of the “Byte-Sized JavaScript” videocasts.
+ *
+ * You can watch “Byte-Sized JavaScript” at: https://bytesized.tv/
+ *
+ * MIT Licensed — See LICENSE.md
+ *
+ * Send your comments, suggestions, and feedback to me@volkan.io
  */
 
 const { join } = require('path');
