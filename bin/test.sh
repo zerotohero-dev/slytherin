7359362d86eda750ac95ea3395538faee49acb85
Volkan Özçelik <volkan.ozcelik@gmail.com>
2018-01-04 23:36:56 -0800

    Update test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+#    _    _
+# ,-(|)--(|)-.
+# \_   ..   _/ Slytherin: Framework-independent slithering utility.
+#   \______/              Makes things draggable.
+#     V  V
+#
+# This project is a part of the “Byte-Sized JavaScript” videocasts.
+#
+# You can watch “Byte-Sized JavaScript” at: https://bytesized.tv/
+#
+# MIT Licensed — See LICENSE.md
+#
+# Send your comments, suggestions, and feedback to me@volkan.io
+#
+
 yarn run lint
 
 if [ $? -eq 1 ]
