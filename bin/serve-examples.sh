7bd64bce1af3fb4af9f465235fed13bae9eff90e
Volkan Özçelik <volkan.ozcelik@gmail.com>
2018-01-04 23:36:43 -0800

    Update serve-examples.sh

--- bin/serve-examples.sh
+++ bin/serve-examples.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
+#    _    _
+# ,-(|)--(|)-.
+# \_   ..   _/ Slytherin: Framework-independent slithering utility.
+#   \______/              Makes things draggable.
+#     V  V
+#
+# This project is a part of the “Byte-Sized JavaScript” videocasts.
+#
+# You can watch “Byte-Sized JavaScript” at: https://bytesized.tv/
+#
+# MIT Licensed — See LICENSE.md
+#
+# Send your comments, suggestions, and feedback to me@volkan.io
+#
+
 http-server ./examples
