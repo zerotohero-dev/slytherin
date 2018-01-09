3a1163f218ef26ee87376572217ea93dbc6ebb20
Volkan Özçelik <me@volkan.io>
2018-01-09 15:56:43 -0800

    minor

---
+++ bin/flow.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#  __.-._
+#  '-._"7'  JediFocus
+#   /'.-c
+#   |  /T   Do. Or do not.
+#  _)_/LI   There is no try.
+#
+#  This project is a part of the “Byte-Sized JavaScript” videocasts.
+#  You can watch “Byte-Sized JavaScript” at: https://bytesized.tv/
+#
+#  MIT Licensed — See LICENSE.md
+#
+#  Send your comments, suggestions, and feedback to me@volkan.io
+
+flow
